4acd90cc45aea3d51aad69491366196b15c5ac5c
Alstonargodi <55945729+Alstonargodi@users.noreply.github.com>
2022-02-23 17:15:09 +0700

    first

---
+++ app/src/main/java/com/example/githubuser/Splashscreen.kt
@@ -0,0 +1,23 @@
+package com.example.githubuser
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import androidx.core.content.ContextCompat
+
+class Splashscreen : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splashscreen)
+
+        Handler(Looper.getMainLooper()).postDelayed({
+            startActivity(Intent(this, MainActivity::class.java))
+        },3000)
+
+
+        window.statusBarColor = ContextCompat.getColor(this,R.color.maincolor)
+        window.navigationBarColor = ContextCompat.getColor(this,R.color.subcolor)
+    }
+}
