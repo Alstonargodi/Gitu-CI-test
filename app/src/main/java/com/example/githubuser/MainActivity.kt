4ac20d4ab269ea935c98faccaa01bc6833ea8ddb
Alstonargodi <55945729+Alstonargodi@users.noreply.github.com>
2022-02-24 12:44:05 +0700

    clean

--- app/src/main/java/com/example/githubuser/MainActivity.kt
+++ app/src/main/java/com/example/githubuser/MainActivity.kt
@@ -3,18 +3,17 @@ package com.example.githubuser
 import android.content.res.Configuration
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
 import androidx.core.content.ContextCompat
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.LinearLayoutManager
-import com.example.githubuser.Model.User
-import com.example.githubuser.View.Home.Userhomerecadapter
+import com.example.githubuser.model.User
+import com.example.githubuser.view.home.Userhomerecadapter
 import com.example.githubuser.databinding.ActivityMainBinding
 
 
 class MainActivity : AppCompatActivity() {
-    lateinit var adapter : Userhomerecadapter
-    lateinit var bind : ActivityMainBinding
+    private lateinit var adapter : Userhomerecadapter
+    private lateinit var bind : ActivityMainBinding
 
 
     private var data = ArrayList<User>()
@@ -24,27 +23,29 @@ class MainActivity : AppCompatActivity() {
         setContentView(bind.root)
 
         adapter = Userhomerecadapter()
-        val recview = bind.recviewUser
-        recview.adapter = adapter
+        val recView = bind.recviewUser
+        recView.adapter = adapter
 
 
         if (applicationContext.resources.configuration.orientation == Configuration.ORIENTATION_LANDSCAPE){
-            recview.layoutManager = GridLayoutManager(this,2)
+            recView.layoutManager = GridLayoutManager(this,2)
         }else{
-            recview.layoutManager = LinearLayoutManager(this)
+            recView.layoutManager = LinearLayoutManager(this)
         }
 
         Inituser()
 
         window.statusBarColor = ContextCompat.getColor(this,R.color.maincolor)
         window.navigationBarColor = ContextCompat.getColor(this,R.color.subcolor)
+
+
     }
 
     fun Inituser(){
-        val nama = resources.getStringArray(R.array.name)
-        val image = resources.obtainTypedArray(R.array.avatar)
-        for (i in nama.indices){
-            val datauser = User(
+        val Name = resources.getStringArray(R.array.name)
+        val Photo = resources.obtainTypedArray(R.array.avatar)
+        for (i in Name.indices){
+            val dataUser = User(
                 resources.getStringArray(R.array.name)[i],
                 resources.getStringArray(R.array.username)[i],
                 resources.getStringArray(R.array.location)[i],
@@ -52,11 +53,16 @@ class MainActivity : AppCompatActivity() {
                 resources.getStringArray(R.array.company)[i],
                 resources.getStringArray(R.array.followers)[i],
                 resources.getStringArray(R.array.following)[i],
-                image.getResourceId(i,-1)
+                Photo.getResourceId(i,-1)
             )
 
-            data.add(datauser)
+            data.add(dataUser)
             adapter.setdata(data)
         }
     }
+
+
+
+
 }
+
--- app/src/main/java/com/example/githubuser/view/Splashscreen.kt
+++ app/src/main/java/com/example/githubuser/view/Splashscreen.kt
@@ -1,4 +1,4 @@
-package com.example.githubuser.View
+package com.example.githubuser.view
 
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
@@ -10,13 +10,17 @@ import com.example.githubuser.MainActivity
 import com.example.githubuser.R
 
 class Splashscreen : AppCompatActivity() {
+
+    companion object{
+        const val TIME = 3000L
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splashscreen)
 
         Handler(Looper.getMainLooper()).postDelayed({
             startActivity(Intent(this, MainActivity::class.java))
-        },3000)
+        }, TIME)
 
 
         window.statusBarColor = ContextCompat.getColor(this, R.color.maincolor)
